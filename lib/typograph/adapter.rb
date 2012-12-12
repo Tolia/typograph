2727ab044ba390f078c3b00ac30643e8703bc29e
sterebooster <stereobooster@gmail.com>
2012-12-13 01:03:37 +0200

    Only 53 examples fail

--- lib/typograph/adapter.rb
+++ lib/typograph/adapter.rb
@@ -4,12 +4,47 @@ require 'htmlentities'
 
 module Typograph
   class Adapter
+
+    SPECIAL = {
+      :lsquo => '‘',
+      :rsquo => '’',
+      :sbquo => '‚',
+      :ldquo => '“',
+      :rdquo => '”',
+      :bdquo => '„',
+      :lsaquo =>  '‹',
+      :rsaquo => '›',
+      :quot => '"',
+      :grave => '`',
+      :laquo => '«',
+      :raquo => '»',
+      :acute => '´',
+      :hellip => '…',
+      :ndash => '–',
+      :mdash => '—',
+      :nbsp => "\xA0",
+      :shy => "\xAD",
+      :cent => '¢',
+      :pound => '£',
+      :curren => '¤',
+      :yen => '¥',
+      :sect => '§',
+      :sup2 => '²',
+      :sup3 => '³'
+    }
+
     def initialize(options={})
       @options = options
     end
 
     def process(text)
 
+    end
+
+    def sym_to_char(sym)
+      res = SPECIAL[sym]
+      raise ArgumentError, "Unknown sym #{sym}" unless res
+      res
     end
 
     # Приводим символы в строке к единой форме для последующей обработки
